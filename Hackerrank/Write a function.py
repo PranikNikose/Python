98c17cb618196d0b43dc11d565b77a51064479a5
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-06-11 19:24:41 +0530

    Update Write a function.py

--- Hackerrank/Write a function.py
+++ Hackerrank/Write a function.py
@@ -1,10 +1,11 @@
 def is_leap(year):
-    leap = False
+    leap = False 
     
-    if year%4==0:
-        return True
-    else:
-        return False
+    
+    if year%400==0:
+        leap = True;
+    elif year%100!=0 and year%4==0 :
+        leap =  True;
     return leap
 
 year = int(input())
